e07ce5bf7301f59807182562dd1f0b882906747e
Stef Tervelde <stef@steftervelde.nl>
2020-11-25 10:35:01 +0100

    task planning

--- src/classes/task.ts
+++ src/classes/task.ts
@@ -4,18 +4,23 @@ export abstract class Task {
   get name() {
     return this.constructor.name;
   }
-  abstract get targets(): RoomPosition[];
-
-  abstract get workers(): number;
-  abstract get priority(): Array<number>;
-
-  abstract get progress(): Progress;
-
-  abstract action(creep: Creep): void;
+  target: RoomPosition;
+  workers: number = 0;
+  priority: Array<Number> = [1];
+  constructor(target: RoomPosition) {
+    this.target = target;
+  }
+  static generate(): Array<Task> {
+    return [];
+  }
+  abstract candidates(creeps: Array<Creep>): Array<Creep>;
 
   abstract finished(creep: Creep): boolean;
+  abstract action(creep: Creep): void;
+
+  abstract get progress(): Progress;
 }
 
-interface Progress {
+export interface Progress {
   finishedIn: number;
 }
--- src/tasks/harvest/energy.ts
+++ src/tasks/harvest/energy.ts
@@ -1,3 +1,28 @@
 // find an unused energy source, one that has not been covered or reserved by any other creeps
 // priority low
 // requested workers 2 / based on the energy needs
+
+import { Task, Progress } from "../../classes/task";
+export class HarvestEnergy extends Task {
+  static generate(): Task[] {
+    return [];
+  }
+  get workers(): number {
+    throw new Error("Method not implemented.");
+  }
+  get priority(): number[] {
+    throw new Error("Method not implemented.");
+  }
+  action(creep: Creep): void {
+    throw new Error("Method not implemented.");
+  }
+  finished(creep: Creep): boolean {
+    throw new Error("Method not implemented.");
+  }
+  candidates(creeps: Creep[]): Creep[] {
+    throw new Error("Method not implemented.");
+  }
+  get progress(): Progress {
+    throw new Error("Method not implemented.");
+  }
+}
